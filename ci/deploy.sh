9cb396418690709e9e585fe3e7d19be776b173a2
Vaughan Crole <44250771+vorno-crole@users.noreply.github.com>
2020-04-08 13:48:51 +1000

    Feature/travis ci (#4)
    * move auth to function
    
    * many things
    
    * fix check
    
    * fix again
    
    Co-authored-by: Vaughan Crole <vaughan.crole@au1.ibm.com>

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -33,6 +33,7 @@ export -f usage
   PRE_DEST="false"
   DEPLOY_WAIT="33"
   AUTO_CI="false"
+  MAIN_BRANCH="false"
 
   while [ $# -gt 0 ] ; do
     case $1 in
@@ -62,10 +63,43 @@ export -f usage
     read -p "Press Enter to continue."
   }
   export -f pause
+
+  authUrlKey()
+  {
+    ### Get url key and authenticate
+    CI_ORG_FILE="ci/auth-url.txt"
+    if [[ $URL_KEY == "" ]]; then
+      echo -e "Error: URL Key not set.\n"
+      exit 1;
+    fi
+    echo "${URL_KEY}" > ${CI_ORG_FILE}
+
+    sfdx force:auth:sfdxurl:store -f ${CI_ORG_FILE} -a ciorg
+    #sfdx force:org:display -u ciorg
+    rm ${CI_ORG_FILE}
+  }
+  export -f authUrlKey
+
+  checkDestructive()
+  {
+    # Check for destructive changes
+    rm -rf destructive
+    echo -e "${GREEN}* Check for destructive changes${RESTORE}\n"
+    sfpackage -x $CI_CMP_BRANCH $CI_NEW_BRANCH destructive -p 46
+
+    if [ -f destructive/unpackaged/destructiveChanges.xml ]; then
+      echo -e "${GREEN}*** ${RED}Destructive changes found ${GREEN}***"
+    else
+      echo -e "No destructive changes found.\n"
+      rm -rf destructive
+    fi
+  }
+  export -f checkDestructive
 # end functions
 
 echo -e "\n${GREEN}*** ${WHITE}Deploy to Salesforce script v${VERSION}\n${GREEN}* ${WHITE}by vc@vaughancrole.com${RESTORE}\n"
 
+# Display header
 if [[ $AUTO_CI != "false" ]]; then
   echo -e "${GREEN}* Running in CI Automatic mode.${RESTORE}\n"
 fi
@@ -99,18 +133,17 @@ if [[ $AUTO_CI == "false" ]]; then
   exit
 fi
 
+if [[ $CI_BRANCH == 'develop' || $CI_BRANCH == 'validation' || $CI_BRANCH == 'release' || $CI_BRANCH == 'master' ]]; then
+  MAIN_BRANCH="true"
+fi
 
-if [[ $CI_EVENT_TYPE == 'pull_request' ]] && [[ $CI_BRANCH == 'develop' || $CI_BRANCH == 'validation' || $CI_BRANCH == 'release' || $CI_BRANCH == 'master' ]]; then
+
+if [[ $CI_EVENT_TYPE == 'pull_request' ]] && [[ $MAIN_BRANCH == 'true' ]]; then
 
   echo -e "${GREEN}*** ${WHITE}PR into $CI_BRANCH - running simulation test${RESTORE}\n"
 
   ### Get url key and authenticate
-  CI_ORG_FILE="ci/pr-test-url.txt"
-  echo "${URL_KEY}" > ${CI_ORG_FILE}
-
-  sfdx force:auth:sfdxurl:store -f ${CI_ORG_FILE} -a ciorg
-  #sfdx force:org:display -u ciorg
-  rm ${CI_ORG_FILE}
+  authUrlKey
 
   # Run Dynamic metadata rewrite on deploy
   #targets/dynamic-metadata.sh --set ${CI_BRANCH} # TODO fix
@@ -123,39 +156,25 @@ if [[ $CI_EVENT_TYPE == 'pull_request' ]] && [[ $CI_BRANCH == 'develop' || $CI_B
   # Restore dynamic metadata rewrite
   #targets/dynamic-metadata.sh --restore # TODO fix
 
-elif [[ $CI_EVENT_TYPE == 'push' ]] && [[ $CI_BRANCH == 'develop' || $CI_BRANCH == 'validation' || $CI_BRANCH == 'release' || $CI_BRANCH == 'master' ]]; then
+elif [[ $CI_EVENT_TYPE == 'push' ]] && [[ $MAIN_BRANCH == 'true' ]]; then
 
   echo -e "${GREEN}*** ${WHITE}Push into $CI_BRANCH - running deploy${RESTORE}\n"
 
   ### Get url key and authenticate
-  CI_ORG_FILE="ci/pr-test-url.txt"
-  echo "${URL_KEY}" > ${CI_ORG_FILE}
-
-  sfdx force:auth:sfdxurl:store -f ${CI_ORG_FILE} -a ciorg
-  #sfdx force:org:display -u ciorg
-  rm ${CI_ORG_FILE}
-
+  authUrlKey
 
   # Run Dynamic metadata rewrite on deploy
   #targets/dynamic-metadata.sh --set ${CI_BRANCH} # TODO fix
 
 
-  # (pre-deploy) destructive changes
-  rm -rf destructive
+  ### Destructive changes
   if [[ $DESTRUCT == 'true' ]]; then
-    echo -e "${GREEN}* Check for destructive changes${RESTORE}\n"
-    sfpackage -x $CI_CMP_BRANCH $CI_NEW_BRANCH destructive -p 46
+    checkDestructive
 
-    if [ -f destructive/unpackaged/destructiveChanges.xml ]; then
-      echo -e "${GREEN}*** ${RED}Destructive changes found ${GREEN}***"
-
-      if [[ $PRE_DEST == 'true' ]]; then
-        echo -e "\n${GREEN}* Pre-deploy: Push destructive changes${RESTORE}\n"
-        sfdx force:mdapi:deploy --deploydir=destructive/unpackaged --ignoreerrors --ignorewarnings --wait=-1 -u ciorg
-      fi
-    else
-      echo -e "No destructive changes found.\n"
-      rm -rf destructive
+    # Pre-deploy push destructive changes
+    if [[ -f destructive/unpackaged/destructiveChanges.xml ]] && [[ $PRE_DEST == 'true' ]]; then
+      echo -e "\n${GREEN}* Pre-deploy: Push destructive changes${RESTORE}\n"
+      sfdx force:mdapi:deploy --deploydir=destructive/unpackaged --ignoreerrors --ignorewarnings --wait=-1 -u ciorg
     fi
   fi
 
@@ -168,7 +187,7 @@ elif [[ $CI_EVENT_TYPE == 'push' ]] && [[ $CI_BRANCH == 'develop' || $CI_BRANCH 
   #pause
 
 
-  # (post-deploy) destructive changes
+  # Post-deploy push destructive changes
   if [[ -f destructive/unpackaged/destructiveChanges.xml && $PRE_DEST == 'false' ]]; then
     echo -e "\n${GREEN}* Post-deploy: Push destructive changes${RESTORE}\n"
     sfdx force:mdapi:deploy --deploydir=destructive/unpackaged --ignoreerrors --ignorewarnings --wait=-1 -u ciorg
