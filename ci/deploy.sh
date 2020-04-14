2c8ddd07febd08ae4a42ec0ec760b50e192f976a
Vaughan Crole <44250771+vorno-crole@users.noreply.github.com>
2020-04-14 21:56:18 +1000

    Develop (#8)
    * add yaml
    
    * add yaml (#1)
    
    Co-authored-by: Vaughan Crole <vaughan.crole@au1.ibm.com>
    
    * rename yaml
    
    * update yml
    
    * add travis env vars
    
    * add auto mode. enable caching
    
    * fix cache
    
    * colours
    
    * remove node_js
    
    * executable
    
    * add url
    
    * add PR support
    
    * typos
    
    * remove url file dependency
    
    * tidy, automate
    
    * Feature/travis ci (#4)
    
    * move auth to function
    
    * many things
    
    * fix check
    
    * fix again
    
    Co-authored-by: Vaughan Crole <vaughan.crole@au1.ibm.com>
    
    * Feature/travis ci (#5)
    
    * move auth to function
    
    * many things
    
    * fix check
    
    * fix again
    
    * Update README.md
    
    * Enable dynamic metadata deployment
    
    * fix eof
    
    * Update README.md
    
    * Update README.md
    
    * set executable
    
    * Update deploy.sh
    
    * Add sub module SF Packager
    
    * turn off crashing
    
    * Don't clone sf-packager
    
    * fix urls
    
    Co-authored-by: Vaughan Crole <vaughan.crole@au1.ibm.com>
    
    * Feature/script tidy (#6)
    
    * error handling
    
    * add branch
    
    * Update deploy.sh
    
    * Tidy
    
    * tidy
    
    * revert
    
    Co-authored-by: Vaughan Crole <vaughan.crole@au1.ibm.com>
    
    * Feature/script tidy (#7)
    
    * error handling
    
    * add branch
    
    * Update deploy.sh
    
    * Tidy
    
    * tidy
    
    * revert
    
    * Update README.md
    
    * Update README.md
    
    Co-authored-by: Vaughan Crole <vaughan.crole@au1.ibm.com>
    
    Co-authored-by: Vaughan Crole <vaughan.crole@au1.ibm.com>

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,20 +2,17 @@
 SECONDS=0
 
 # Set up env vars here
-  set -e
   GREEN="\033[32;1m"
   WHITE="\033[97;1m"
   RED="\033[91;1m"
   RESTORE="\033[0m"
 
-  CI_EVENT_TYPE="push"
-  CI_BRANCH="$(git symbolic-ref --short -q HEAD 2>/dev/null)"
+  CI_EVENT_TYPE="${TRAVIS_EVENT_TYPE}"
+  CI_BRANCH="${TRAVIS_BRANCH}"
   CI_NEW_BRANCH="${CI_BRANCH}"
   CI_CMP_BRANCH="${CI_BRANCH}^"
-  CI_PULL_REQUEST="false"
-  CI_PULL_REQUEST_BRANCH=""
-
-  CI_URL_PATH="../keys" # TODO fix
+  CI_PULL_REQUEST="${TRAVIS_PULL_REQUEST}"
+  CI_PULL_REQUEST_BRANCH="${TRAVIS_PULL_REQUEST_BRANCH}"
 # end set up env vars
 
 usage()
@@ -27,18 +24,16 @@ usage()
 export -f usage
 
 # args
-  VERSION="1.0 b30"
+  VERSION="1.1 b31"
   REAL_DEPLOY="false"
   TEST_CODE="-l RunLocalTests"
   VALI_FLAG="-c"
   DESTRUCT="true"
   PRE_DEST="false"
   DEPLOY_WAIT="33"
-
-  # if [[ $# -eq 0 ]]; then
-  #   usage
-  #   exit 0
-  # fi
+  AUTO_CI="false"
+  MAIN_BRANCH="false"
+  REWRITE="false"
 
   while [ $# -gt 0 ] ; do
     case $1 in
@@ -50,6 +45,7 @@ export -f usage
       --destruct-pre-deploy) PRE_DEST="true" ;;
       -s | --skip-code-coverage) TEST_CODE="" ;;
       -q | --queue-deploy) DEPLOY_WAIT="0" ;;
+      --automatic) AUTO_CI="true" ;;
       -h | --help) usage
                    exit 0;;
       *) echo -e "${RED}*** ERROR: ${RESTORE}Invalid option: ${WHITE}$1${RESTORE}. See usage:"
@@ -67,24 +63,75 @@ export -f usage
     read -p "Press Enter to continue."
   }
   export -f pause
+
+  authUrlKey()
+  {
+    ### Get url key and authenticate
+    CI_ORG_FILE="ci/auth-url.txt"
+    if [[ $URL_KEY == "" ]]; then
+      echo -e "Error: URL Key not set.\n"
+      exit 1;
+    fi
+    echo "${URL_KEY}" > ${CI_ORG_FILE}
+
+    sfdx force:auth:sfdxurl:store -f ${CI_ORG_FILE} -a ciorg
+    #sfdx force:org:display -u ciorg
+    rm ${CI_ORG_FILE}
+  }
+  export -f authUrlKey
+
+  checkDestructive()
+  {
+    # Check for destructive changes
+    rm -rf destructive
+    echo -e "${GREEN}* Check for destructive changes${RESTORE}\n"
+    sfpackage -x $CI_CMP_BRANCH $CI_NEW_BRANCH destructive -p 46
+
+    if [ -f destructive/unpackaged/destructiveChanges.xml ]; then
+      echo -e "${GREEN}*** ${RED}Destructive changes found ${GREEN}***"
+    else
+      echo -e "No destructive changes found.\n"
+      rm -rf destructive
+    fi
+  }
+  export -f checkDestructive
+
+  function finish
+  {
+    EXIT_CODE=$?
+    if [[ $REWRITE == "true" ]]; then
+      # Restore dynamic metadata rewrite
+      targets/dynamic-metadata.sh --restore
+    fi
+
+    if [[ $EXIT_CODE > 0 ]]; then
+      echo -e "${RED}*** Error. Stopping script.${RESTORE}"
+    fi
+
+    echo -e "${GREEN}* ${WHITE}Deploy Script End."
+    echo -e "Time taken: ${SECONDS} seconds.\n"
+    exit $EXIT_CODE
+  }
+  trap finish EXIT
 # end functions
 
 echo -e "\n${GREEN}*** ${WHITE}Deploy to Salesforce script v${VERSION}\n${GREEN}* ${WHITE}by vc@vaughancrole.com${RESTORE}\n"
 
+# Display header
+if [[ $AUTO_CI != "false" ]]; then
+  echo -e "${GREEN}* Running in CI Automatic mode.${RESTORE}\n"
+fi
 echo "Event: $CI_EVENT_TYPE"
 echo "Branch: $CI_BRANCH"
 if [[ $CI_NEW_BRANCH != $CI_BRANCH ]]; then
   echo "New Branch: $CI_NEW_BRANCH"
 fi
-echo "Compare Branch: $CI_CMP_BRANCH"
 echo "Pull Request: $CI_PULL_REQUEST"
 if [[ $CI_PULL_REQUEST != "false" ]]; then
   echo "PR Branch: $CI_PULL_REQUEST_BRANCH"
+  CI_CMP_BRANCH="${CI_PULL_REQUEST_BRANCH}"
 fi
-
-if [[ $TEST_CODE == "" ]]; then
-  echo "Skip Test Coverage"
-fi
+echo "Compare Branch: $CI_CMP_BRANCH"
 
 if [[ $DESTRUCT == "true" ]]; then
   echo "Destructive changes check enabled."
@@ -98,56 +145,54 @@ else
   echo "Destructive changes check disabled."
 fi
 
-if [[ $REAL_DEPLOY == 'true' ]]; then
-  echo -e "${GREEN}*** ${RED}Deploying for real ${GREEN}***"
-  VALI_FLAG=""
-else
-  echo "Simulation deployment."
-fi
-
 echo ""
 
+if [[ $AUTO_CI == "false" ]]; then
+  exit
+fi
 
-if [[ $CI_EVENT_TYPE == 'push' ]] && [[ $CI_BRANCH == 'develop' || $CI_BRANCH == 'validation' || $CI_BRANCH == 'release' || $CI_BRANCH == 'master' ]]; then
+if [[ $CI_BRANCH == 'develop' || $CI_BRANCH == 'validation' || $CI_BRANCH == 'release' || $CI_BRANCH == 'master' ]]; then
+  MAIN_BRANCH="true"
+fi
 
-  echo -e "${GREEN}*** ${WHITE}Push into $CI_BRANCH - running deploy\n"
-  echo -e "\n${GREEN}* ${WHITE}Authenticate org\n"
 
-  echo -e "* ${WHITE}Looking for deploy $CI_BRANCH url"
+if [[ $CI_EVENT_TYPE == 'pull_request' ]] && [[ $MAIN_BRANCH == 'true' ]]; then
 
-  if [ -f $CI_URL_PATH/deploy-$CI_BRANCH-url.txt ]; then
-    echo -e "File $CI_URL_PATH/deploy-$CI_BRANCH-url.txt found.\n"
-  else
-    echo -e "\n${GREEN}*** ${WHITE}Error: File not found.\n"
-    exit 1;
-  fi
+  echo -e "${GREEN}*** ${WHITE}PR into $CI_BRANCH - running simulation test${RESTORE}\n"
 
-  sfdx force:auth:sfdxurl:store -f $CI_URL_PATH/deploy-$CI_BRANCH-url.txt -a ciorg
-  #sfdx force:org:display -u ciorg
-
-  echo -e "${RESTORE}"
-
+  ### Get url key and authenticate
+  authUrlKey
 
   # Run Dynamic metadata rewrite on deploy
-  #targets/dynamic-metadata.sh --set ${CI_BRANCH} # TODO fix
+  REWRITE="true"
+  targets/dynamic-metadata.sh --set ${CI_BRANCH}
+
+  #####
+  # Run simulation deployment
+  #####
+  sfdx force:source:deploy --checkonly --testlevel=RunLocalTests --sourcepath=force-app/main/default --wait=${DEPLOY_WAIT} -u ciorg
 
 
-  # (pre-deploy) destructive changes
-  rm -rf destructive
+elif [[ $CI_EVENT_TYPE == 'push' ]] && [[ $MAIN_BRANCH == 'true' ]]; then
+
+  echo -e "${GREEN}*** ${WHITE}Push into $CI_BRANCH - running deploy${RESTORE}\n"
+
+  ### Get url key and authenticate
+  authUrlKey
+
+  # Run Dynamic metadata rewrite on deploy
+  REWRITE="true"
+  targets/dynamic-metadata.sh --set ${CI_BRANCH}
+
+
+  ### Destructive changes
   if [[ $DESTRUCT == 'true' ]]; then
-    echo -e "${GREEN}* Check for destructive changes${RESTORE}\n"
-    sfpackage -x $CI_CMP_BRANCH $CI_NEW_BRANCH destructive -p 46
+    checkDestructive
 
-    if [ -f destructive/unpackaged/destructiveChanges.xml ]; then
-      echo -e "${GREEN}*** ${RED}Destructive changes found ${GREEN}***"
-
-      if [[ $PRE_DEST == 'true' ]]; then
-        echo -e "\n${GREEN}* Pre-deploy: Push destructive changes${RESTORE}\n"
-        sfdx force:mdapi:deploy ${VALI_FLAG} --deploydir=destructive/unpackaged --ignoreerrors --ignorewarnings --wait=-1 -u ciorg
-      fi
-    else
-      echo -e "No destructive changes found.\n"
-      rm -rf destructive
+    # Pre-deploy push destructive changes
+    if [[ -f destructive/unpackaged/destructiveChanges.xml ]] && [[ $PRE_DEST == 'true' ]]; then
+      echo -e "\n${GREEN}* Pre-deploy: Push destructive changes${RESTORE}\n"
+      sfdx force:mdapi:deploy --deploydir=destructive/unpackaged --ignoreerrors --ignorewarnings --wait=-1 -u ciorg
     fi
   fi
 
@@ -155,31 +200,17 @@ if [[ $CI_EVENT_TYPE == 'push' ]] && [[ $CI_BRANCH == 'develop' || $CI_BRANCH ==
   #####
   # Run main deployment
   #####
-  if [[ $REAL_DEPLOY == 'true' ]]; then
-    echo -e "${GREEN}*** ${RED}Real deploy ${GREEN}***${RESTORE}\n"
-  else
-    echo -e "\n${GREEN}* Simulation deploy${RESTORE}\n"
-  fi
-  sfdx force:source:deploy ${VALI_FLAG} ${TEST_CODE} -p force-app/main/default --wait=${DEPLOY_WAIT} -u ciorg
-  #pause
+  echo -e "${GREEN}*** ${RED}Real deploy ${GREEN}***${RESTORE}\n"
+  sfdx force:source:deploy --testlevel=RunLocalTests --sourcepath=force-app/main/default --wait=${DEPLOY_WAIT} -u ciorg
 
 
-  # (post-deploy) destructive changes
+  # Post-deploy push destructive changes
   if [[ -f destructive/unpackaged/destructiveChanges.xml && $PRE_DEST == 'false' ]]; then
     echo -e "\n${GREEN}* Post-deploy: Push destructive changes${RESTORE}\n"
-    sfdx force:mdapi:deploy ${VALI_FLAG} --deploydir=destructive/unpackaged --ignoreerrors --ignorewarnings --wait=-1 -u ciorg
+    sfdx force:mdapi:deploy --deploydir=destructive/unpackaged --ignoreerrors --ignorewarnings --wait=-1 -u ciorg
   fi
-
-
-  # Restore dynamic metadata rewrite
-  #targets/dynamic-metadata.sh --restore # TODO fix
-
 
 else
   echo -e "${GREEN}*** ${WHITE}No action required.\n"
 
 fi
-
-echo -e "${GREEN}* ${WHITE}Deploy Script End."
-echo -e "Time taken: ${SECONDS} seconds.\n"
-exit 0;
